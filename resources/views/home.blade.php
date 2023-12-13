9d618ef8185e64f47d4249fe9105df3418d1449b
Mr Farday <mr.farday@MacBook-Pro-Mr.local>
2023-12-13 09:06:59 +0700

    eps 3Struktur Folder, Router & View

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>MJ BLOG | Home</title>
+</head>
+<body>
+    <h1>Halaman Home</h1> 
+    <h3>Muhammad Jauhari</h3>
+    <p>Juahari@gmail.com</p>
+    <img src="" alt="">
+<script src="js/script.js"></script>
+</body>
+</html>
